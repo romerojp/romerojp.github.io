24664acc0020d0a3a388257766ed56a1ef72e55f
Juan Pablo Romero <jeipi.romero@gmail.com>
2020-04-20 15:53:52 -0500

    Update main.js

--- TornTools/js/main.js
+++ TornTools/js/main.js
@@ -2,4 +2,7 @@ function start() {
   var GET=[];
   window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,function(a,name,value){GET[name]=value;});
   console.log(GET);
+  if (typeof GET['APIkey'] !== 'undefined' || GET['APIkey'] !== null) {
+    document.getElementById('APIkey').value = GET['APIkey'];
+  }
 }
